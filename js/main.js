c41f01f87dc90ec352beddaaf03b30bf07badb4a
Dan Taeyoung <provolot@gmail.com>
2017-04-05 00:53:23 -0400

    whitespace play

--- js/main.js
+++ js/main.js
@@ -13,55 +13,55 @@ var getUrlValue = function(VarSearch){
 }
 
 function updateUrlWithData() {
-	var savedata = {};
-	$(".editableelem").each(function(i, el) {
-		savedata[el.id] = el.innerHTML;
-	});
-	var url = location.protocol + '//' + location.host + location.pathname;
-	history.replaceState('', '', url + "?" +  $.param(savedata));
+  var savedata = {};
+  $(".editableelem").each(function(i, el) {
+    savedata[el.id] = el.innerHTML;
+  });
+  var url = location.protocol + '//' + location.host + location.pathname;
+  history.replaceState('', '', url + "?" +  $.param(savedata));
 }
 
 //Creating dynamic link that automatically click
 function downloadURI(uri, name) {
-		var link = document.createElement("a");
-		link.download = name;
-		link.href = uri;
-		link.click();
-		//after creating link you should delete dynamic link
-		//clearDynamicLink(link); 
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    link.click();
+    //after creating link you should delete dynamic link
+    //clearDynamicLink(link); 
 }
 
 //Your modified code.
 function saveAsImage(div, filename) {
-		html2canvas(div, {
-				dpi: 300,
-				onrendered: function (canvas) {
+    html2canvas(div, {
+        dpi: 300,
+        onrendered: function (canvas) {
           canvas.toBlob(function(blob) {
             saveAs(blob, filename);
           });
-				}
-		});
+        }
+    });
 }
 
 
 $(function() {
 
-	$(".editableelem").each(function(i, el) {
-		if(getUrlValue(el.id) != undefined) {
-			el.innerHTML = decodeURIComponent(getUrlValue(el.id));
-		}
-	});
-	$(".editableelem").each(function(i, el) {
-		$(el).editable({type: "textarea", action: "click"}, function(e){
-		  updateUrlWithData()
-		});
-	});
+  $(".editableelem").each(function(i, el) {
+    if(getUrlValue(el.id) != undefined) {
+      el.innerHTML = decodeURIComponent(getUrlValue(el.id));
+    }
+  });
+  $(".editableelem").each(function(i, el) {
+    $(el).editable({type: "textarea", action: "click"}, function(e){
+      updateUrlWithData()
+    });
+  });
 
 
 
   $("button#saveas").click(function() {
     var filename = $("#quiz_title").html().replace(/[^a-z0-9]/gi, '_').toLowerCase();
-		saveAsImage($("#twobytwo_wrapper"), filename);
-  })	
+    saveAsImage($("#twobytwo_wrapper"), filename);
+  })  
 
 });
